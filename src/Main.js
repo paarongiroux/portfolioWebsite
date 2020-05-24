f1aec84a9bd4f97f9a98faad5f97c65e5b3965ba
Aaron <pag87@nau.edu>
2020-05-24 15:22:10 -0700

    removes about page

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './home.js';
-import About from './about.js';
 import Projects from './Projects.js';
 import Contact from './Contact.js';
 
@@ -11,7 +10,6 @@ const Main = () => {
     <BrowserRouter>
         <Switch> {/* The Switch decides which component to show based on the current URL.*/}
             <Route exact path='/' component={Home}></Route>
-            <Route exact path='/about' component={About}></Route>
             <Route exact path='/projects' component={Projects}></Route>
             <Route exact path='/contact' component={Contact}></Route>
         </Switch>
--- src/NavBar.js
+++ src/NavBar.js
@@ -10,7 +10,6 @@ class NavBar extends React.Component {
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"></link>
                 <ul className = "navul">
                     <li className = "navli"><Link to="/">Home</Link></li>
-                    <li className = "navli"><Link to="/about">About</Link></li>
                     <li className = "navli"><Link to="/projects">Projects</Link></li>
                     <li className = "navli"><Link to="/contact">Contact</Link></li>
                     <li className = "navli"><a href="http://linkedin.com/in/pagiroux"><i class="fa fa-linkedin"></i></a></li>
